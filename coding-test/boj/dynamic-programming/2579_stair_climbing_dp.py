9abbf3864196838710c628c6e4ab7247eb5dcbbe
choiminji <chlals9930@gmail.com>
2025-12-29 21:35:48 +0900

    [완료] DP Top-Down으로 풀이

--- coding-test/boj/dynamic-programming/2579_stair_climbing_dp.py
+++ coding-test/boj/dynamic-programming/2579_stair_climbing_dp.py
@@ -1,31 +1,39 @@
 '''
 BOJ 2579 - Stair Climbing
 Category: Dynamic Programming
-Approach: Top-Down
+Approach: Bottom-Up 
+* Bottom-up
+- for무으로 i= 1 -> N까지 순서대로 채움
+*Top-Down
+- 재귀 + 메모이제이션 
 - Top-Down은 "전체 상태 공간"을 채우려는 것이 아니라, "정답을 구성하는 데 실제로 필요한 상태만" 호출합니다.
 - 정답을 만들기 위한 하위 문제를 요청합니다. 
 '''
 
+'''
+문제 힌트 
+dp[i][cnt] = i번째 계단에 있고, 연속으로 cnt번 밟은 상태에서 얻을 수 있는 최대 점수 
+'''
+
 import sys 
-input = int(sys.stdin.readline())
 
 def solve():   # 총 점수의 최댓값 구하기
-    N = input.strip()
-    table = [0] + [input.strip() for _ in range(N)]
-    dp = [-1 for _ in range(N+1)]
+    N = int(sys.stdin.readline())
+    table = [0] + [int(sys.stdin.readline()) for _ in range(N)]
+    dp = [[0]*3 for _ in range(N+1)] 
 
-    def fib(n):
-        if n <= 1:   # fib 정의, fib(0) = 0, fib(1) = 1
-            return table[n]
-    
-        if dp[n] != -1:  # dp[n]에 값이 저장되어 있으면, 그 값을 바로 반환 [중복 계산 방지]
-            return dp[n]
-        
-        # 최댓값(2칸 올라갔을 때, 1칸 올라갔을 때)를 구해준다. 
-        fib(n) = max(fib(n-2) + table[n], fib(n-1) + table[n])
+    dp[1][1] = table[1]
+    if N >= 2:
+        dp[2][1] = table[2]
+        dp[2][2] = table[1] + table[2]
 
-        # 3번 연속해서 올라갔을 때는 어떻게 해결할까? 
+    for i in range(3, N+1):  # 3부터 N까지
+        # 1칸 올라갔을 때 
+        dp[i][2] = dp[i-1][1] + table[i]
+        # 2칸 올라갔을 때
+        dp[i][1] = max(dp[i-2][1], dp[i-2][2]) + table[i]
 
-    fib(N)
-
+    print(max(dp[N][1], dp[N][2]))
 solve()
+
+
