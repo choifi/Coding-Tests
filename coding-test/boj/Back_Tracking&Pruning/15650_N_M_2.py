aa77e9d5ab816f9f63d289a0ffa852aaa1d151d0
choiminji <chlals9930@gmail.com>
2026-01-18 16:33:21 +0900

    success

--- coding-test/boj/Back_Tracking&Pruning/15650_N_M_2.py
+++ coding-test/boj/Back_Tracking&Pruning/15650_N_M_2.py
@@ -15,17 +15,20 @@ path = []
 # start는 다음에 뽑을 수 있는 최소 숫자. 
 
 def dfs(depth, start): # depth는 지금까지 뽑은 개수 
-    global cur
+    # 종료 조건
     if depth == M:
         print(*path)
         return
     
+    # 선택 루프 (오름차순 + 사전순)
     for x in range(start, N+1):
+        # 선택
         path.append(x)
 
+        # 다음 단계 
         dfs(depth + 1, x + 1) # 다음 숫자는 반드시 더 커야 하므로 x+1부터 시작합니다. 
 
-        # backtracking
+        # 되돌리기 - backtracking
         path.pop()
 
 dfs(0,1)
