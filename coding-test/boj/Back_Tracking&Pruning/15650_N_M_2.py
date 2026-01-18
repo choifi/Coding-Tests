2c4f585c060f922ecb914b90b2057b47727d4d35
choiminji <chlals9930@gmail.com>
2026-01-18 16:30:11 +0900

    success

--- coding-test/boj/Back_Tracking&Pruning/15650_N_M_2.py
+++ coding-test/boj/Back_Tracking&Pruning/15650_N_M_2.py
@@ -4,34 +4,28 @@
 [조건]
 중복 없이 M개를 고른 수열
 고른 수열은 오름차순
+
+[ N과 M(1) 문제와 다른 점 ]
+고른 수열은 오름차순 처리 [start]를 해 줌. start는 "오름차순 조건 + 중복 제거"를 담당한다. 따라서 visited가 필요없다. 
 '''
 
 import sys 
 N, M = map(int, sys.stdin.readline().split())
-
-visited = [False] * (N+1)
 path = []
-cur = 0
+# start는 다음에 뽑을 수 있는 최소 숫자. 
 
-def dfs(depth):
+def dfs(depth, start): # depth는 지금까지 뽑은 개수 
     global cur
     if depth == M:
         print(*path)
-        cur = 0
         return
     
-    for i in range(1, N+1):
-        if visited[i] or cur >= i:
-            continue
+    for x in range(start, N+1):
+        path.append(x)
 
-        visited[i] = True
-        path.append(i)
-        cur = i
-
-        dfs(depth + 1)
+        dfs(depth + 1, x + 1) # 다음 숫자는 반드시 더 커야 하므로 x+1부터 시작합니다. 
 
         # backtracking
         path.pop()
-        visited[i] = False
 
-dfs(0)
+dfs(0,1)
